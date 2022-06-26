56d853073db7c554c0c71cfa0d3a3656d266ff3e
Anna Toczek <ania4134@gmail.com>
2022-06-26 19:53:21 +0100

    22.3 rozszerzenie klasy odpowiedzi

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,6 +1,5 @@
 package com.crud.tasks.service;
 
-import com.crud.tasks.controller.GlobalHttpErrorHandler;
 import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
@@ -8,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
