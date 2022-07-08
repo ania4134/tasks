2d9c760b435c2f2cfa739e8cf6550cd9055cb4e9
Anna Toczek <ania4134@gmail.com>
2022-07-08 18:57:44 +0100

    SimpleEmailService class modified

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -33,9 +33,9 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-//        if(Optional.of(mail.getMailToCC()).isPresent()) {
-//            mailMessage.setCc(mail.getMailToCC());
-//        }
+        if(Optional.ofNullable(mail.getMailToCC()).isPresent()) {
+            mailMessage.setCc(mail.getMailToCC());
+        }
         return mailMessage;
     }
 }
